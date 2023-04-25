a8e9741b59d109d0df5cc02364f61dcf07235c6f
Gökhan Öztürk <2248968+wurucu@users.noreply.github.com>
2023-04-25 22:03:35 +0300

    (test) import cycler err3

--- services/redis/redis.go
+++ services/redis/redis.go
@@ -3,7 +3,9 @@ package redis
 import (
 	"context"
 	"fmt"
+	"github.com/InsuranceTech/shared/common"
 	"github.com/InsuranceTech/shared/common/depth"
+	"github.com/InsuranceTech/shared/common/period"
 	"github.com/InsuranceTech/shared/common/symbol"
 	"github.com/InsuranceTech/shared/config"
 	"github.com/redis/go-redis/v9"
@@ -48,50 +50,50 @@ func GetkeyDepth(symbol *symbol.Symbol) string {
 	return fmt.Sprintf("%s:Depth", symbol.ToStringNoPeriod())
 }
 
-//func SaveCandleSeries(periodicSeries *cm.PeriodicCandleSeries, period period.Period) bool {
-//	series := periodicSeries.Get(period)
-//	if series == nil {
-//		return false
-//	}
-//	bytes, err := series.MarshalMsg(nil)
-//	if err != nil {
-//		panic(err)
-//	}
-//
-//	key := GetkeyCandles(periodicSeries.Symbol.CloneP(period))
-//	status := Client.Set(context.Background(), key, bytes, 0)
-//	if status.Err() != nil {
-//		panic(status.Err())
-//	}
-//	return true
-//}
+func SaveCandleSeries(periodicSeries *common.PeriodicCandleSeries, period period.Period) bool {
+	series := periodicSeries.Get(period)
+	if series == nil {
+		return false
+	}
+	bytes, err := series.MarshalMsg(nil)
+	if err != nil {
+		panic(err)
+	}
 
-//func SaveCandleSeriesAsync(periodicSeries *cm.PeriodicCandleSeries, period period.Period, onComplete func(status bool)) {
-//	go func() {
-//		status := SaveCandleSeries(periodicSeries, period)
-//		if onComplete != nil {
-//			onComplete(status)
-//		}
-//	}()
-//}
+	key := GetkeyCandles(periodicSeries.Symbol.CloneP(period))
+	status := Client.Set(context.Background(), key, bytes, 0)
+	if status.Err() != nil {
+		panic(status.Err())
+	}
+	return true
+}
 
-//func GetCandleSeries(symbol *symbol.Symbol) (*cm.CandleSeries, error) {
-//	key := GetkeyCandles(symbol)
-//	cmdStatus := Client.Get(context.Background(), key)
-//	if cmdStatus.Err() != nil {
-//		return nil, cmdStatus.Err()
-//	}
-//	bytes, err := cmdStatus.Bytes()
-//	if err != nil {
-//		return nil, err
-//	}
-//	series := &cm.CandleSeries{}
-//	_, err = series.UnmarshalMsg(bytes)
-//	if err != nil {
-//		return nil, err
-//	}
-//	return series, nil
-//}
+func SaveCandleSeriesAsync(periodicSeries *common.PeriodicCandleSeries, period period.Period, onComplete func(status bool)) {
+	go func() {
+		status := SaveCandleSeries(periodicSeries, period)
+		if onComplete != nil {
+			onComplete(status)
+		}
+	}()
+}
+
+func GetCandleSeries(symbol *symbol.Symbol) (*common.CandleSeries, error) {
+	key := GetkeyCandles(symbol)
+	cmdStatus := Client.Get(context.Background(), key)
+	if cmdStatus.Err() != nil {
+		return nil, cmdStatus.Err()
+	}
+	bytes, err := cmdStatus.Bytes()
+	if err != nil {
+		return nil, err
+	}
+	series := &common.CandleSeries{}
+	_, err = series.UnmarshalMsg(bytes)
+	if err != nil {
+		return nil, err
+	}
+	return series, nil
+}
 
 func SaveDepthData(symbol *symbol.Symbol, depthData *depth.DepthData) error {
 	key := GetkeyDepth(symbol)
