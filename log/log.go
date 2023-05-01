1ffa4368925fece459aa7ccb246502f2fd2cab20
Gökhan Öztürk <2248968+wurucu@users.noreply.github.com>
2023-05-01 04:34:03 +0300

    (change) log text template

--- log/log.go
+++ log/log.go
@@ -42,53 +42,53 @@ func CreateTag(tagName string) *TaggedLogger {
 
 // region Taglı kullanım
 func (l *TaggedLogger) InfoF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("INFO", text, l.Tag)
 }
 
 func (l *TaggedLogger) Info(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("INFO", text, l.Tag)
 }
 
 func (l *TaggedLogger) WarningF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("WARNING", text, l.Tag)
 }
 
 func (l *TaggedLogger) Warning(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("WARNING", text, l.Tag)
 }
 
 func (l *TaggedLogger) ErrorF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("ERROR", text, l.Tag)
 }
 
 func (l *TaggedLogger) Error(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("ERROR", text, l.Tag)
 }
 
 func (l *TaggedLogger) LogF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("LOG", text, l.Tag)
 }
 
 func (l *TaggedLogger) Log(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("LOG", text, l.Tag)
 }
 
 func (l *TaggedLogger) FatalF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("FATAL", text, l.Tag)
 	os.Exit(1)
 }
 
 func (l *TaggedLogger) Fatal(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("FATAL", text, l.Tag)
 	os.Exit(1)
 }
@@ -97,53 +97,53 @@ func (l *TaggedLogger) Fatal(a ...any) {
 
 // region Genel kullanım
 func InfoF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("INFO", text, "")
 }
 
 func Info(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("INFO", text, "")
 }
 
 func WarningF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("WARNING", text, "")
 }
 
 func Warning(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("WARNING", text, "")
 }
 
 func ErrorF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("ERROR", text, "")
 }
 
 func Error(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("ERROR", text, "")
 }
 
 func LogF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("LOG", text, "")
 }
 
 func Log(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("LOG", text, "")
 }
 
 func FatalF(format string, a ...any) {
-	text := fmt.Sprintf(format, a)
+	text := fmt.Sprintf(format, a...)
 	write("FATAL", text, "")
 	os.Exit(1)
 }
 
 func Fatal(a ...any) {
-	text := fmt.Sprint(a)
+	text := fmt.Sprint(a...)
 	write("FATAL", text, "")
 	os.Exit(1)
 }
