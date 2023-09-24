93e4fd55225b19e046d5029dfcf2c7b7f7b1af7f
Gökhan Öztürk <2248968+wurucu@users.noreply.github.com>
2023-09-24 04:07:22 +0300

    (feat) utils/extension func FromModelSymbolsPeriod

--- common/extension/SymbolEx.go
+++ common/extension/SymbolEx.go
@@ -8,6 +8,10 @@ import (
 )
 
 func FromModelSymbols(symbols []*model.Symbol) []*symbol.Symbol {
+	return FromModelSymbolsPeriod(symbols, period.NonePeriod)
+}
+
+func FromModelSymbolsPeriod(symbols []*model.Symbol, period period.Period) []*symbol.Symbol {
 	retSymbols := make([]*symbol.Symbol, 0)
 	if len(symbols) == 0 {
 		return retSymbols
@@ -15,13 +19,13 @@ func FromModelSymbols(symbols []*model.Symbol) []*symbol.Symbol {
 	for _, s := range symbols {
 		switch s.ExchangeType {
 		case int(symbol.BINANCE_SPOT):
-			retSymbols = append(retSymbols, symbol.NewBinanceSpotSymbol(s.Name, period.NonePeriod))
+			retSymbols = append(retSymbols, symbol.NewBinanceSpotSymbol(s.Name, period))
 			break
 		case int(symbol.BINANCE_USDT_FUTURE):
-			retSymbols = append(retSymbols, symbol.NewBinanceUsdtFutureSymbol(s.Name, period.NonePeriod))
+			retSymbols = append(retSymbols, symbol.NewBinanceUsdtFutureSymbol(s.Name, period))
 			break
 		case int(symbol.BINANCE_COIN_FUTURE):
-			retSymbols = append(retSymbols, symbol.NewBinanceCoinFutureSymbol(s.Name, period.NonePeriod))
+			retSymbols = append(retSymbols, symbol.NewBinanceCoinFutureSymbol(s.Name, period))
 			break
 		}
 	}
