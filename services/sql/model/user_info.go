92b0390510f446c1b49e5956741cf8105d676cf4
alex.zakarov <semerci394@gmail.com>
2024-01-26 10:58:09 -0800

    hotfix(db): resolve user_lang_code relation

--- services/sql/model/user_info.go
+++ services/sql/model/user_info.go
@@ -2,6 +2,6 @@ package model
 
 type UserLangCode struct {
 	tableName struct{} `pg:"auth.users"`
-	ID        int32    `pg:"id,pk"`
+	ID        int64    `pg:"id,pk" json:"id,omitempty"`
 	UserLang  string   `pg:"lang_code" json:"lang_code,omitempty"`
 }
