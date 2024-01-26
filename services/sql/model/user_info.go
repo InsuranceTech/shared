9cfa34556307faabdc4a655a8b2829bc4fef6c03
alex.zakarov <semerci394@gmail.com>
2024-01-26 12:32:13 -0800

    bugfix(sql): resolve user info relations

--- services/sql/model/user_info.go
+++ services/sql/model/user_info.go
@@ -1,16 +1,15 @@
 package model
 
 type UserInfo struct {
-	tableName struct{}    `pg:"auth.users"`
-	ID        int64       `pg:"id,pk" json:"id,omitempty"`
-	UserLang  string      `pg:"lang_code" json:"lang_code,omitempty"`
-	FcmTokens []*FcmToken `pg:"rel:has-many,fk:user_id"`
+	tableName struct{} `pg:"auth.users"`
+	ID        int64    `pg:"id,pk" json:"id,omitempty"`
+	UserLang  string   `pg:"lang_code" json:"lang_code,omitempty"`
 }
 
 type FcmToken struct {
 	tableName struct{} `pg:"auth.fcm_tokens"`
 	ID        int64    `pg:"id,pk" json:"id,omitempty"`
-	UserID    int      `pg:"user_id,fk" json:"user_id,omitempty"`
+	UserID    int      `pg:"user_id" json:"user_id,omitempty"`
 	TypeOf    string   `pg:"type_of" json:"type_of,omitempty"`
 	Token     string   `pg:"fcm_token" json:"fcm_token,omitempty"`
 	IsAct     bool     `pg:"is_act" json:"is_act,omitempty"`
