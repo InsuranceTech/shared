9fac6a7fd5345aa6c769597dec37c42de5a4b599
alex.zakarov <semerci394@gmail.com>
2024-01-26 13:26:16 -0800

    bugfix(sql): resolve user info relations

--- services/sql/model/user_info.go
+++ services/sql/model/user_info.go
@@ -8,10 +8,10 @@ type UserInfo struct {
 }
 
 type FcmToken struct {
-	tableName struct{} `pg:"fcm_tokens"`
-	ID        int64    `pg:"id,pk"`
-	UserID    int64    `pg:"user_id"`
-	TypeOf    string   `pg:"type_of"`
-	Token     string   `pg:"fcm_token"`
-	IsAct     bool     `pg:"is_act"`
+	tableName struct{} `pg:"auth.fcm_tokens"`
+	ID        int64    `pg:"id,pk" json:"id,omitempty"`
+	UserID    int64    `pg:"user_id" json:"user_id,omitempty"`
+	TypeOf    string   `pg:"type_of" json:"type_of,omitempty"`
+	Token     string   `pg:"fcm_token" json:"fcm_token,omitempty"`
+	IsAct     bool     `pg:"is_act" json:"is_act,omitempty"`
 }
