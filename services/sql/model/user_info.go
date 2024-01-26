d190164defc01f6e2e55c54a861b0d1bd4064cc6
alex.zakarov <semerci394@gmail.com>
2024-01-26 10:13:57 -0800

    feat(alarms): add user language code for i18n

--- services/sql/model/user_info.go
+++ services/sql/model/user_info.go
@@ -2,5 +2,6 @@ package model
 
 type UserLangCode struct {
 	tableName struct{} `pg:"auth.users"`
+	ID        int32    `pg:"id,pk"`
 	UserLang  string   `pg:"lang_code" json:"lang_code,omitempty"`
 }
